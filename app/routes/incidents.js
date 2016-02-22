e2f641b2297de27fbf89e811bd66b3ec32175435
Khanh Hua <giakhanh2487@gmail.com>
2016-02-22 08:22:38 +0000

    Added test cases

--- app/routes/incidents.js
+++ app/routes/incidents.js
@@ -10,12 +10,30 @@ exports.GET = function getIncidents (req, res) {
 exports.POST = function postIncidents (req, res) {
   var data = req.body;
   
+  if (!data) {
+    return res.status(400).send('Invalid data');
+  }
+  
+  if (!('type' in data)) {
+    return res.status(400).send('Invalid data');
+  }
+  
+  if (!('location' in data)) {
+    return res.status(400).send('Invalid data');
+  }
+  
+  if (!('latlng' in data.location)) {
+    return res.status(400).send('Invalid data');
+  }
+  
   var incident = new Incident(data);
 
   incident.save().then(function(incident) {
     console.log('[controller.incident.POST] Incident has been saved');
+    // TODO Send a task item to RabbitMQ here
+    
     res.send(incident);    
   }).catch(function(error) {
-    res.send(400, error.message);
+    res.status(400).send(error.message);
   });
 };
--- app/routes/incidents.spec.js
+++ app/routes/incidents.spec.js
@@ -30,7 +30,7 @@ describe('Incident controller: POST', function () {
             model.prototype = {
                 save: function () { 
                     console.warn('Should be overriden with sinon.mock');
-                    return Promise.resolve();
+                    throw new Error('Not implemented');
                 }
             };
             
@@ -38,12 +38,17 @@ describe('Incident controller: POST', function () {
         }());
         // mock the error reporter
         mockery.enable({
-          warnOnReplace: false,
-          warnOnUnregistered: false,
-          useCleanCache: true
+            warnOnReplace: false,
+            warnOnUnregistered: false,
+            useCleanCache: true
         });
         
         incidents = require('./incidents');
+    });
+    
+    afterEach(function () {
+        // var Incident = require('../models/incident');
+        // Incident.prototype.save.restore(); 
     });
     
     after(function() {
@@ -55,7 +60,10 @@ describe('Incident controller: POST', function () {
         expect(typeof(incidents.POST)).equal('function');
     });
     
-    it('resonds to /', function (done) {
+    it('creates new incident properly', function (done) {
+        // Why it's called unittest?
+        // Because we know all about the internal implementation of the unit.
+        
         var Incident = require('../models/incident');
         
         var mockedIncident = {
@@ -65,7 +73,9 @@ describe('Incident controller: POST', function () {
             location: {
                 latlng: [1,2],
                 spec: {
-                    placeid: '123'
+                    placeid: '123',
+                    administrative_area_level_1: 'Ho Chi Minh City',
+                    country: 'Vietnam'
                 }
             }
         };
@@ -78,22 +88,164 @@ describe('Incident controller: POST', function () {
                 location: {
                     latlng: [1,2],
                     spec: {
-                        placeid: '123'
+                        placeid: '123',
+                        administrative_area_level_1: 'Ho Chi Minh City',
+                        country: 'Vietnam'
                     }
                 }
             }
         };
         var res = {};
         res.send = sinon.spy();
-        
+        // Perform the tested action
         incidents.POST(req, res);
         
-        setTimeout(function() {
+        process.nextTick(function() {
             expect(Incident.prototype.save.calledOnce).to.equal(true);
             // TODO Fix the assertion
             expect(res.send.calledOnce).to.equal(true);
-            
+            Incident.prototype.save.restore(); 
             done();
-        }, 0);
+        });
+    });
+    
+    it('generates error for improper incident data: No JSON body', function (done) {
+        // Why it's called unittest?
+        // Because we know all about the internal implementation of the unit.
+        
+        var Incident = require('../models/incident');
+        sinon.stub(Incident.prototype, 'save').returns(Promise.reject({ message: 'Invalid data' }));
+        
+        var req = {
+            body: null
+        };
+        var send = sinon.spy();
+        var status = sinon.stub().returns({ send: send }) ;
+        var res = {
+            status: status
+        };
+        
+        // Perform the tested action
+        incidents.POST(req, res);
+        
+        process.nextTick(function() {
+            expect(Incident.prototype.save.callCount).to.equal(0);
+            // TODO Fix the assertion
+            expect(status.calledWith(400)).to.equal(true);
+            expect(send.calledWith('Invalid data')).to.equal(true);
+           
+            Incident.prototype.save.restore();  
+            done();
+        });
+    });
+    
+    it('generates error for improper incident data: No type', function (done) {
+        // Why it's called unittest?
+        // Because we know all about the internal implementation of the unit.
+        
+        var Incident = require('../models/incident');
+        sinon.stub(Incident.prototype, 'save').returns(Promise.reject({ message: 'Invalid data' }));
+        
+        var req = {
+            body: {
+                location: {
+                    latlng: [1,2],
+                    spec: {
+                        placeid: '123',
+                        administrative_area_level_1: 'Ho Chi Minh City',
+                        country: 'Vietnam'
+                    }
+                }
+            }
+        };
+        var send = sinon.spy();
+        var status = sinon.stub().returns({ send: send }) ;
+        var res = {
+            status: status
+        };
+        
+        // Perform the tested action
+        incidents.POST(req, res);
+        
+        process.nextTick(function() {
+            expect(Incident.prototype.save.callCount).to.equal(0);
+            // TODO Fix the assertion
+            expect(status.calledWith(400)).to.equal(true);
+            expect(send.calledWith('Invalid data')).to.equal(true);
+           
+            Incident.prototype.save.restore();  
+            done();
+        });
+    });
+    
+    it('generates error for improper incident data: No latlng', function (done) {
+        // Why it's called unittest?
+        // Because we know all about the internal implementation of the unit.
+        
+        var Incident = require('../models/incident');
+        sinon.stub(Incident.prototype, 'save').returns(Promise.reject({ message: 'Invalid data' }));
+        
+        var req = {
+            body: {
+                type: 'accident',
+                location: {
+                    spec: {
+                        placeid: '123',
+                        administrative_area_level_1: 'Ho Chi Minh City',
+                        country: 'Vietnam'
+                    }
+                }
+            }
+        };
+        var send = sinon.spy();
+        var status = sinon.stub().returns({ send: send }) ;
+        var res = {
+            status: status
+        };
+        
+        // Perform the tested action
+        incidents.POST(req, res);
+        
+        process.nextTick(function() {
+            expect(Incident.prototype.save.callCount).to.equal(0);
+            // TODO Fix the assertion
+            expect(status.calledWith(400)).to.equal(true);
+            expect(send.calledWith('Invalid data')).to.equal(true);
+           
+            Incident.prototype.save.restore();  
+            done();
+        });
+    });
+    
+    it('generates error for improper incident data: No location', function (done) {
+        // Why it's called unittest?
+        // Because we know all about the internal implementation of the unit.
+        
+        var Incident = require('../models/incident');
+        sinon.stub(Incident.prototype, 'save').returns(Promise.reject({ message: 'Invalid data' }));
+        
+        var req = {
+            body: {
+                type: 'accident'
+            }
+        };
+        var send = sinon.spy();
+        var status = sinon.stub().returns({ send: send }) ;
+        var res = {
+            status: status
+        };
+        
+        // Perform the tested action
+        incidents.POST(req, res);
+        
+        process.nextTick(function() {
+            expect(Incident.prototype.save.callCount).to.equal(0);
+            // TODO Fix the assertion
+            expect(status.calledWith(400)).to.equal(true);
+            expect(send.calledWith('Invalid data')).to.equal(true);
+           
+            Incident.prototype.save.restore();  
+            done();
+        });
     });
 });
